cd56b0015cd8fa94f1a345d7c00602b6ea2b7617
Gilberto <comparan77@gmail.com>
2019-09-06 12:54:05 -0500

    ready

--- public/js/views/recepcion.js
+++ public/js/views/recepcion.js
@@ -6,21 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
         defaultView: 'timeGridWeek',
         locale: 'es',
         themeSystem: 'bootstrap',
-        events: [
-            {
-              title  : 'event1',
-              start  : '2019-09-05T15:15:00'
-            },
-            {
-              title  : 'event2',
-              start  : '2019-09-07T13:30:00'
-            },
-            {
-              title  : 'event3',
-              start  : '2019-09-09T12:30:00',
-              allDay : false // will make the time show
-            }
-          ]
+        events: "http://localhost:3002/asn_schedule",
+        allDay : false // will make the time show        
     });
 
     calendar.render();
